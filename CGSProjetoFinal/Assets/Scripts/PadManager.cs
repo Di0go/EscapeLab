d5987aae021bf17869e0262952cd83d32d299237
Diogo <diogopinto301@gmail.com>
2022-05-03 21:20:34 +0100

    Sequence Prototype

---
+++ CGSProjetoFinal/Assets/Scripts/PadManager.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+//using Linq here to be able to call SequenceEqual, it apparently allows you to compare references
+using System.Linq;
+
+public class PadManager : Sequence
+{
+    Renderer padRenderer;
+    GameObject parentObj;
+
+    private void Start()
+    {
+        //to only change the pressed pad's material
+        padRenderer = GetComponent<Renderer>();
+
+        //to change all of the pads materials
+        parentObj = transform.parent.gameObject;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if the pad wasn't already added to the list 
+        if (!playerOrder.Contains(int.Parse(transform.name)))
+        {
+            //add it to the list
+            playerOrder.Add(int.Parse(transform.name));
+
+            //changes the pads material to yellow uppon activation
+            padRenderer.material.color = Color.yellow;
+
+            Debug.Log("Pad " + transform.name + " added!");
+
+            //sequence is correct
+            if (playerOrder.Count == correctOrder.Count && playerOrder.SequenceEqual(correctOrder))
+            {
+                ChangePadsColor(parentObj, Color.green);
+            }
+
+            //sequence is incorrect
+            else if (playerOrder.Count == correctOrder.Count && !playerOrder.SequenceEqual(correctOrder))
+            {
+                ChangePadsColor(parentObj, Color.red);
+            }
+        }
+        else if (playerOrder.Contains(int.Parse(transform.name)))
+        {
+            Debug.Log("Pad " + transform.name + " was already pressed!");
+        }
+    }
+
+    //changes the pads color when the sequence is completed
+    private void ChangePadsColor(GameObject parent, Color col)
+    {
+        //loops trough every child object that the parent has
+        foreach (Transform child in parent.transform)
+        {
+            //changes it's color
+            child.GetComponent<Renderer>().material.color = col;
+        }
+    }
+
+}
---
+++ CGSProjetoFinal/Assets/Scripts/Sequence.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Sequence : MonoBehaviour
+{
+    static protected List<int> correctOrder;
+
+    static protected List<int> playerOrder;
+
+    void Start()
+    {
+        //defines the correct order for the sequence
+        correctOrder = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        //this list is used to store the sequence chosen by the player
+        playerOrder = new List<int>();
+    }
+}
