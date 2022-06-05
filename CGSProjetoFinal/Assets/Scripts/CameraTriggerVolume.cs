0d1ad330250a807ab2b632ce4dad8fb9ba9b303f
RaulHorta <100358850+RaulHorta@users.noreply.github.com>
2022-06-05 16:23:05 +0100

    Iluminação + Cameras

---
+++ CGSProjetoFinal/Assets/Scripts/CameraTriggerVolume.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(Rigidbody))]
+public class CameraTriggerVolume : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cam;
+    [SerializeField] private Vector3 boxSize;
+
+    BoxCollider box;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        box = GetComponent<BoxCollider>();
+        rb = GetComponent<Rigidbody>();
+        box.isTrigger = true;
+        box.size = boxSize;
+
+        rb.isKinematic = true;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(transform.position, boxSize);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (CameraSwitcher.ActiveCamera != cam) CameraSwitcher.SwitchCamera(cam);
+            Debug.Log(cam.name + "Area");
+        }
+    }
+}
