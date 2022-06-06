0da41d06e40a1710e5e35e738ffb4c989fd34611
RaulHorta <100358850+RaulHorta@users.noreply.github.com>
2022-06-06 11:58:20 +0100

    Map adjustments

--- CGSProjetoFinal/Assets/Scripts/Sequence System/Sequence.cs
+++ CGSProjetoFinal/Assets/Scripts/Sequence System/Sequence.cs
@@ -10,7 +10,8 @@ public class Sequence : MonoBehaviour
     void Start()
     {
         //defines the correct order for the sequence
-        correctOrder = new List<int> {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        correctOrder = new List<int> {3, 6, 1, 7, 2, 5, 4, 9, 8};
+        //1, 2, 3, 4, 5, 6, 7, 8, 9          
 
         //this list is used to store the sequence chosen by the player
         playerOrder = new List<int>();
